956e6c9bda48b22bcafac2143a2539188d2752fb
rs5634nr <rphillips2256@gmail.com>
2017-03-08 17:46:40 -0600

    New Stuff

--- GameLife/src/GameOfLife/Main.java
+++ GameLife/src/GameOfLife/Main.java
@@ -1,17 +1,16 @@
 package GameOfLife;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	private Stage primaryStage;
-	private BorderPane mainLayout;
+	private Pane mainLayout;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
