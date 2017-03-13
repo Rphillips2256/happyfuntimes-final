d4e0f7954b7a5582405084ff7b136a03969492c1
rs5634nr <rphillips2256@gmail.com>
2017-03-13 16:59:01 -0500

    newest

--- GameLife/src/GameOfLife/Main.java
+++ GameLife/src/GameOfLife/Main.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-//change
+//change to this line
 public class Main extends Application {
 	
 	private Stage primaryStage;
